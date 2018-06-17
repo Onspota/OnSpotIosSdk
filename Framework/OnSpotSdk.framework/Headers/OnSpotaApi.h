620cc37e66356ec4f07ac461ff88fcfc2e3cde97
Tomer Lavi <tomer@assist.co.il>
2018-06-17 16:40:22 +0300

    First commit

---
+++ Framework/OnSpotSdk.framework/Headers/OnSpotaApi.h
@@ -0,0 +1,17 @@
+//
+//  OnSpotaApi.h
+//  OnSpota
+//
+//  Created by Tomer Lavi on 06/02/2018.
+//  Copyright © 2018 onspota. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OnSpotaApi : NSObject
+
+@property NSString* apiKey;
+
+- (void) callSearchApi:(NSDictionary *)params completionHandler:(void (^)(NSDictionary* result))completionHandler;
+
+@end
